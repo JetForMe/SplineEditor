43b03425736ef8464d5d289ae93ada3e3e35f59e
Rick Mann <rmann@latencyzero.com>
2021-05-02 20:56:20 -0700

    Initial Commit

---
+++ SplineEditor/SplineEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplineEditorApp.swift
+//  SplineEditor
+//
+//  Created by Rick Mann on 2021-05-02.
+//
+
+import SwiftUI
+
+@main
+struct SplineEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
